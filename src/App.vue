b73378b15649068bd3860a42dfd6bcfa2b660a67
Windix Feng <windix@gmail.com>
2025-04-18 09:24:25 +1000

    feat: block auth01

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,36 @@
-<script setup lang="ts"></script>
-
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+</script>
 <template>
-  <main>
-    <div>Hello</div>
-  </main>
+  <div class="w-full h-screen flex items-center justify-center px-4">
+    <Card class="w-full max-w-sm">
+      <CardHeader>
+        <CardTitle class="text-2xl"> Login </CardTitle>
+        <CardDescription> Enter your email below to login to your account. </CardDescription>
+      </CardHeader>
+      <CardContent class="grid gap-4">
+        <div class="grid gap-2">
+          <Label for="email">Email</Label>
+          <Input id="email" type="email" placeholder="m@example.com" required />
+        </div>
+        <div class="grid gap-2">
+          <Label for="password">Password</Label>
+          <Input id="password" type="password" required />
+        </div>
+      </CardContent>
+      <CardFooter>
+        <Button class="w-full"> Sign in </Button>
+      </CardFooter>
+    </Card>
+  </div>
 </template>
