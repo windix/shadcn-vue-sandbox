9a8d1e95796160717aea04b8b7cb69d66016b528
Windix Feng <windix@gmail.com>
2025-04-18 11:03:06 +1000

    feat: add index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const router = useRouter()
+</script>
 
 <template>
-  <div>Hello</div>
+  <h1 class="text-2xl m-5">Shadcn Vue Demo</h1>
+  <main class="flex flex-col gap-4 ml-5 mr-5">
+    <Button class="w-20" @click="router.push('/sidebar')">Sidebar</Button>
+    <Button class="w-20" @click="router.push('/login')">Login</Button>
+  </main>
 </template>
