225c74a005c4a9b7570a65144465d3afaef7dfbd
Windix Feng <windix@gmail.com>
2025-04-18 10:15:44 +1000

    fix: auto loaded vue components

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,6 @@ export const iframeHeight = '800px'
 export const description = 'A simple sidebar with navigation grouped by section.'
 </script>
 <script setup lang="ts">
-import AppSidebar from '@/components/AppSidebar.vue'
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from '@/components/ui/breadcrumb'
-import { Separator } from '@/components/ui/separator'
-import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 </script>
 <template>
   <SidebarProvider>
